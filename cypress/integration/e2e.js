b4a18e3b612d882383d5c612429928e3c303d11e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-02-01 10:08:16 -0500

    add more tests

---
+++ cypress/integration/e2e.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+it('counts', () => {
+  cy.visit('/')
+  cy.contains('[data-cy=count]', '0')
+  cy.get('[data-cy=add]').click().click()
+  cy.contains('[data-cy=count]', '2')
+  cy.contains('button', 'Double').click()
+  cy.contains('[data-cy=count]', '4')
+})
